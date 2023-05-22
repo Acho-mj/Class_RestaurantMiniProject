c0a14f71fe5528ea651f699b4c6bd6b2e2a01127
Choi MinJung <mj020503@naver.com>
2023-05-22 20:21:24 +0900

    feat: Menu delete sort

--- src/miniproject/Main.java
+++ src/miniproject/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         System.out.println("<< 음식점 주문 시스템 >>");
         while (true) {
-            System.out.println("메뉴 목록:");
+            System.out.println("=== 메뉴 목록 ===");
             Menu[] menuList = restaurant.getMenuList();
             
             for (int i = 0; i < menuList.length; i++) {
@@ -20,61 +20,99 @@ public class Main {
                 }
             }
             
-            System.out.print("주문할 메뉴 번호를 입력하세요 (종료하려면 0 입력): ");
+            System.out.println("\n1. 메뉴 추가");
+            System.out.println("2. 메뉴 삭제");
+            System.out.println("3. 주문하기");
+            System.out.println("0. 종료");
+            System.out.print("원하는 작업을 선택하세요: ");
             int choice = sc.nextInt();
-
-            if (choice == 0) {
+            
+            switch (choice) {
+            case 0:
+                System.out.println("프로그램을 종료합니다.");
+                sc.close();
+                return;
+            case 1:
+                System.out.print("추가할 메뉴의 이름을 입력하세요: ");
+                String name = sc.next();
+                System.out.print("추가할 메뉴의 가격을 입력하세요: ");
+                double price = sc.nextDouble();
+                Menu newMenu = new Menu(name, price);
+                boolean isAdded = restaurant.addMenu(newMenu);
+                if (isAdded) {
+                    System.out.println(newMenu.getName() + " 메뉴가 추가되었습니다.\n");
+                } else {
+                    System.out.println("이미 존재하는 메뉴입니다.\n");
+                }
                 break;
-            } else if (choice >= 1 && choice <= menuList.length) {
-                System.out.print("수량을 입력하세요: ");
-                int num = sc.nextInt();
-                Menu menuChoice = menuList[choice - 1];
-                
-                if (menuChoice != null) {
-                    for (int i = 0; i < num; i++) {
-                        table.addOrder(menuChoice);
+            case 2:
+            	System.out.print("삭제할 메뉴의 번호를 입력하세요: ");
+                int menuNumber = sc.nextInt();
+                boolean isDeleted = restaurant.deleteMenu(menuNumber);
+                if (isDeleted) {
+                    System.out.println("메뉴가 삭제되었습니다.\n");
+                } else {
+                    System.out.println("유효하지 않은 메뉴 번호입니다.\n");
+                }
+                break;           
+            case 3:
+            	System.out.print("주문할 메뉴 번호를 입력하세요 (종료하려면 0 입력): ");
+                int orderChoice = sc.nextInt();
+                if (orderChoice == 0) {
+                    break;
+                } else if (orderChoice >= 1 && orderChoice <= menuList.length) {
+                    System.out.print("수량을 입력하세요: ");
+                    int num = sc.nextInt();
+                    Menu menuChoice = menuList[orderChoice - 1];
+
+                    if (menuChoice != null) {
+                        for (int i = 0; i < num; i++) {
+                            table.addOrder(menuChoice);
+                        }
+
+                        System.out.println(menuChoice.getName() + " " + num + "개가 주문되었습니다.\n");
+                    } else {
+                        System.out.println("유효하지 않은 선택입니다. 다시 선택하세요.");
                     }
-                    
-                    System.out.println(menuChoice.getName() + " " + num + "개가 주문되었습니다.\n");
                 } else {
                     System.out.println("유효하지 않은 선택입니다. 다시 선택하세요.");
                 }
-            } else {
+                System.out.println("\n == 주문 내역 ==");
+                Menu[] orderList = table.getOrderList();
+
+                // 주문된 메뉴의 개수를 저장하기 위한 배열
+                int[] menuCounts = new int[orderList.length];
+
+                // 각 메뉴별로 주문된 개수를 카운트
+                for (int i = 0; i < orderList.length; i++) {
+                	Menu menu = orderList[i];
+                	int count = 1;
+
+                	// 현재 메뉴와 동일한 메뉴가 뒤에 중복해서 주문되었는지 확인
+                	for (int j = i + 1; j < orderList.length; j++) {
+                		if (menu == orderList[j]) {
+                			count++;
+                		}
+                	}
+
+                	menuCounts[i] = count;
+                }
+
+                // 중복을 제외하고 주문 내역 출력
+                for (int i = 0; i < orderList.length; i++) {
+                	if (i > 0 && orderList[i] == orderList[i - 1]) {
+                		continue; // 중복된 메뉴는 출력하지 않음
+                	}
+
+                	Menu menu = orderList[i];
+                	int count = menuCounts[i];
+                	System.out.println(menu.getName() + ", " + count + "\n");
+                }
+                break;
+            default:
                 System.out.println("유효하지 않은 선택입니다. 다시 선택하세요.");
+                break;
             }
         }
-
-        System.out.println("\n == 주문 내역 ==");
-        Menu[] orderList = table.getOrderList();
-
-        // 주문된 메뉴의 개수를 저장하기 위한 배열
-        int[] menuCounts = new int[orderList.length];
-
-        // 각 메뉴별로 주문된 개수를 카운트
-        for (int i = 0; i < orderList.length; i++) {
-        	Menu menu = orderList[i];
-        	int count = 1;
-
-        	// 현재 메뉴와 동일한 메뉴가 뒤에 중복해서 주문되었는지 확인
-        	for (int j = i + 1; j < orderList.length; j++) {
-        		if (menu == orderList[j]) {
-        			count++;
-        		}
-        	}
-
-        	menuCounts[i] = count;
-        }
-
-        // 중복을 제외하고 주문 내역 출력
-        for (int i = 0; i < orderList.length; i++) {
-        	if (i > 0 && orderList[i] == orderList[i - 1]) {
-        		continue; // 중복된 메뉴는 출력하지 않음
-        	}
-
-        	Menu menu = orderList[i];
-        	int count = menuCounts[i];
-        	System.out.println(menu.getName() + ", " + count);
-        }
-        sc.close();
-    }
+	}
 }
--- src/miniproject/Restaurant.java
+++ src/miniproject/Restaurant.java
@@ -3,7 +3,8 @@ package miniproject;
 public class Restaurant {
     private Menu[] menuList;
     int max = 100;
-
+    
+    
     public Restaurant() {
         menuList = new Menu[max]; //메뉴 100개로 제한
         
@@ -13,9 +14,56 @@ public class Restaurant {
         menuList[3] = new Menu("닭갈비", 15000);
         menuList[4] = new Menu("떡볶이", 14000);
     }
-
+    
+    // 메뉴 목록 반환
     public Menu[] getMenuList() {
-        return menuList;
+    	// 삭제된 메뉴를 제외한 메뉴 개수 계산
+        int count = 0;
+        for (int i = 0; i < menuList.length; i++) {
+            if (menuList[i] != null) {
+            	count++;
+            }
+        }
+
+        // 삭제된 메뉴를 제외하고 순차적으로 메뉴 목록 재배열
+        Menu[] sortedMenuList = new Menu[count];
+        int index = 0;
+        for (int i = 0; i < menuList.length; i++) {
+            if (menuList[i] != null) {
+                sortedMenuList[index] = menuList[i];
+                index++;
+            }
+        }
+
+        // 재배열된 메뉴 목록 반환
+        return sortedMenuList;
     }
     
+    public boolean addMenu(Menu menu) {
+        for (int i = 0; i < menuList.length; i++) {
+            if (menuList[i] != null && menuList[i].getName().equals(menu.getName())) {
+                return false; // 이미 존재하는 메뉴
+            }
+        }
+
+        for (int i = 0; i < menuList.length; i++) {
+            if (menuList[i] == null) {
+                menuList[i] = menu;
+                return true; // 메뉴 추가 성공
+            }
+        }
+
+        return false; // 더 이상 메뉴를 추가할 수 없음
+    }
+
+    public boolean deleteMenu(int menuNumber) {
+        if (menuNumber >= 1 && menuNumber <= menuList.length) {
+            int index = menuNumber - 1;
+            if (menuList[index] != null) {
+                menuList[index] = null;
+                return true; // 메뉴 삭제 성공
+            }
+        }
+        return false; // 유효하지 않은 메뉴 번호 또는 이미 삭제된 메뉴
+    }
 }

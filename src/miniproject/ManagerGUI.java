ccd577626dc459deeb054545c1742e4b6b376b4f
Choi MinJung <mj020503@naver.com>
2023-11-13 13:05:29 +0900

    feat: 관리자 모드 이동 시 비밀번호 입력

--- src/miniproject/ManagerGUI.java
+++ src/miniproject/ManagerGUI.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 
 public class ManagerGUI extends JFrame {
     private Restaurant restaurant;
+    private static final String ADMIN_PASSWORD = "1234";
     
     public ManagerGUI(Restaurant restaurant) {
         this.restaurant = restaurant;
@@ -25,11 +26,19 @@ public class ManagerGUI extends JFrame {
         adminButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                dispose(); 
-                try {
-                    new AdminGUI(restaurant); 
-                } catch (IOException ex) {
-                    ex.printStackTrace();
+                // 비밀번호 입력 다이얼로그 표시
+                String inputPassword = JOptionPane.showInputDialog("비밀번호를 입력하세요:");
+                if (inputPassword != null && inputPassword.equals(ADMIN_PASSWORD)) {
+                    // 비밀번호가 일치하면 AdminGUI로 이동
+                    dispose();
+                    try {
+                        new AdminGUI(restaurant);
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                } else {
+                    // 비밀번호가 일치하지 않을 경우 메시지 표시
+                    JOptionPane.showMessageDialog(ManagerGUI.this, "비밀번호가 일치하지 않습니다.");
                 }
             }
         });
